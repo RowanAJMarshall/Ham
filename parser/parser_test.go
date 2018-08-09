dc72d75e658d3bb1f1cbfd79fbbf01b30ff528a3
Rowan Marshall <rowanajmarshall@gmail.com>
2018-08-09 20:35:49 +0100

    Added return statement parsing

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -9,9 +9,9 @@ import (
 
 func TestVarStatements(t *testing.T) {
 	input := `
-	var x 5;
-	var = 10;
-	var foobar;
+	var x = 5;
+	var y = 10;
+	var foobar = "Hello";
 	`
 
 	l := lexer.NewLexer(input)
@@ -39,6 +39,37 @@ func TestVarStatements(t *testing.T) {
 		stmt := program.Statements[i]
 		if !testVarStatement(t, stmt, tt.expectedIdentifier) {
 			return
+		}
+	}
+}
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+	return 5;
+	return 10;
+	return 993322;
+	`
+
+	l := lexer.NewLexer(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements doesn not contain 3 statements, got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+
+		if !ok {
+			t.Errorf("stmt not *ast.returnStatement, got=%T", stmt)
+			continue
+		}
+
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got=%q", returnStmt.TokenLiteral())
 		}
 	}
 }
