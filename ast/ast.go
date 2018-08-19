d99a99504cf628d84b35828b7edb909c65ae4b58
Rowan Marshall <rowanajmarshall@gmail.com>
2018-08-19 17:04:55 +0100

    Added support for parsing integers

--- ast/ast.go
+++ ast/ast.go
@@ -83,6 +83,23 @@ type VarStatement struct {
 	Value Expression
 }
 
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode() {
+
+}
+
+func (il *IntegerLiteral) TokenLiteral() string {
+	return il.Token.Literal
+}
+
+func (il *IntegerLiteral) String() string {
+	return il.Token.Literal
+}
+
 func (ls *VarStatement) statementNode() {
 
 }
--- parser/parser.go
+++ parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/rowanajmarshall/ham/ast"
 	"github.com/rowanajmarshall/ham/lexer"
@@ -42,11 +43,39 @@ func New(l *lexer.Lexer) *Parser {
 		errors: []string{},
 	}
 
-	// Read two token, setting both curToken and peekToken
+	// Read two tokens, setting both curToken and peekToken
 	p.nextToken()
 	p.nextToken()
 
+	p.prefixParseFuncs = make(map[token.TokenType]prefixParseFunc)
+
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+
 	return p
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{
+		Token: p.curToken,
+	}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+	return lit
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
 }
 
 func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFunc) {
